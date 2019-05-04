60e4af997091a2678b5e8b5af40db05c65107d4b
Daniël de Kok <me@danieldk.eu>
2019-05-04 15:25:38 +0200

    Add Nix environment

---
+++ default.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "ndarray-tensorflow-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  nativeBuildInputs = [
+    pkgconfig
+    latest.rustChannels.stable.rust
+  ];
+
+  buildInputs = [
+    curl
+    libtensorflow
+    openssl
+  ] ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
+}
